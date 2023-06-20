3193d34b845e6f03f23fbdd6cc23144ae71f7c5a
KiruiTito <titokirui402@gmail.com>
2023-06-20 11:21:23 +0300

    Completed assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,9 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+           def grades
+            students = Student.order(grade: :desc)
+            render json: students
+          end
+        end          
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+get '/students/grades', to: 'students#grades'
+
+get '/students/grades', to: 'students#grades'
+
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+
 end
